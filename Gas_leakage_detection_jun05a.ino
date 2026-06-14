dc877e8ad22824182ec699713760df5593acda88
RifatParadoxical <zikkiz275@gmail.com>
2026-06-14 20:23:08 +0600

    Refactor code structure for improved readability and maintainability

--- Gas_leakage_detection_jun05a.ino
+++ Gas_leakage_detection_jun05a.ino
@@ -1,9 +1,10 @@
-#include "arduino_secrets.h"
+#include "arduino_secrets.h"  // secret header file for WiFi credentials and other sensitive information
 #include <math.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "thingProperties.h"
+#include "voices.h"
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -13,12 +14,21 @@ double b = -0.5;
 double yOne = -0.3980;
 double xOne = 2.9030;
 double rO = 0;
+int calibrated = false;
+unsigned long long connectionStartTime = 0;
 
 void onCommentChange(){
   //
 }
 void onPpmChange(){
   //
+}
+
+void playPCMSound(const unsigned char* sp_CRITICAL, int sp_CRITICAL_LEN){
+    for(int i = 0; i < sp_CRITICAL_LEN; i++){
+        dacWrite(25, pgm_read_byte(&(sp_CRITICAL[i])));
+        delayMicroseconds(1000000 / 16000);
+    }
 }
 
 void loopFunc() {
@@ -50,21 +60,22 @@ void loopFunc() {
 
     if (x < 500) {
       msg = "Air is normal";
-    } else if (x >= 500 && x < 1000) {
-      msg = "Slight LPG detected. Check area.";
-    } else if (x >= 1000 && x < 5000) {
-      msg = "Alarming rise of LPG! Open Windows.";
-    } else if (x >= 5000 && x < 15000) {
-      msg = "DANGER: High LPG! Evacuate!";
+    } else if (x >= 500 && x < 5000) {
+      playPCMSound(sp_CRITICAL, sp_CRITICAL_LEN);
+      msg = "Slight Gas leakage detected. Check area.";
+    } else if (x > 5000) {
+      playPCMSound(sp_CRITICAL, sp_CRITICAL_LEN);
+      msg = "Alarming rise of Gas presence! Open Windows.";
     } else {
-      msg = "CRITICAL: Explosive level! Leave NOW!";
+      msg = "Something went wrong! Check the Sensor.";
     }
     PPM = x;
     Comment = msg;
     lcd.setCursor(0, 0);
-    lcd.print("LPG PPM in Air:      ");
+    lcd.print("Gas in Air(PPM):      ");
     lcd.setCursor(0, 1);
     lcd.print(x);
+    lcd.print("           ");
     Serial.print("Reading Sensor: ");
     Serial.println(gas_value);
     Serial.print("VS: ");
@@ -73,7 +84,7 @@ void loopFunc() {
     Serial.print(rS);
     Serial.print(" | log of X: ");
     Serial.println(log_x);
-    Serial.print("LPG presence in Air (in PPM): ");
+    Serial.print("Gas presence in Air (in PPM): ");
     Serial.println(x);
   }
 }
@@ -88,8 +99,8 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("MQ-5 Heating Up!    ");
   lcd.setCursor(0, 1);
-  lcd.print("Wait 5 minutes      ");
-  delay(300000);
+  lcd.print("Wait a minute...      ");
+  delay(60000);
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
   lcd.clear();
   setDebugMessageLevel(2);
@@ -110,4 +121,10 @@ void loop() {
   ArduinoCloud.update();
   loopFunc();
   delay(500);
+  if((connectionStartTime - millis()) > 86400000 && !calibrated){
+    calibrated = true;
+    double gas = analogRead(sensor);
+    double vO = gas * (3.3 / 4095);
+    rO = (rL * (3.3 - vO)) / vO;
+  }
 }
